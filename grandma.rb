a806ca22e0ec9a06697bece409e1c17eb6aa40cc
Jung Lee <lee.jung.s95@gmail.com>
2020-04-17 03:55:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,4 +18,10 @@ def speak_to_grandma(phrase)
   elsif phrase == "Hi Nana, how are you?"
     return "HUH? SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
-    return 
+    return "HUH? SPEAK UP, SONNY"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  end
+end
