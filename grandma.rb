39f053f7bd63eaf57ebfcb420f0257e6e430f0ab
Jung Lee <lee.jung.s95@gmail.com>
2020-04-17 03:51:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,4 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase=="I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "Hi Nana, how are you?"
+    return "HUH? SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    return 
